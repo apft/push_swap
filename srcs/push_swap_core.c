be90de4e0988e40f7c9136b4dcd1cff05d3b539a
Alexandre PION <apion@student.42.fr>
2019-04-11 17:59:51 +0200

    push_swap: refactor algo

--- srcs/push_swap_core.c
+++ srcs/push_swap_core.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 17:28:11 by apion             #+#    #+#             */
-/*   Updated: 2019/04/11 15:45:35 by apion            ###   ########.fr       */
+/*   Updated: 2019/04/11 16:26:20 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 #include "output.h"
 #include "ft_printf.h"
 
-#define VISUAL	1
+#define VISUAL	0
 
 void	insert_b(t_data *stacks, int value)
 {
-	ft_printf("value = %d, median_b = %d\n", value, stacks->median_b);
+	//ft_printf("value = %d, median_b = %d\n", value, stacks->median_b);
 	pb(stacks);
 	if (value < stacks->median_b)
 		rb(stacks);
@@ -43,33 +43,46 @@ int			push_swap(t_data *stacks)
 {
 	int		count;
 	int		size_a;
+	int		median_a;
 
-	count = 0;
-	size_a = size_stack(stacks->a);
-	stacks->median_a = get_median(stacks->a);
-	stacks->median_b = 0;
-	ft_printf("med_a = %d\n", stacks->median_a);
-	while (!is_sort_stack(stacks->a) && count++ < size_a)
+	if (VISUAL)
+		print_stacks(stacks);
+	//ft_printf("med_a = %d\n", median_a);
+	while (!is_sort_stack(stacks->a))
 	{
-		if (stacks->a->value >= stacks->median_a)
-			ra(stacks);
-		else
-			insert_b(stacks, stacks->a->value);
+		count = 0;
+		size_a = size_stack(stacks->a);
+		median_a = get_median(stacks->a);
+		stacks->median_a = median_a;
+		stacks->median_b = 0;
+		while (!is_sort_stack(stacks->a) && count++ < size_a)
+		{
+			if (stacks->a->value >= median_a)
+				ra(stacks);
+			else
+				insert_b(stacks, stacks->a->value);
+		}
+	}
+	if (!is_sort_stack(stacks->a))
+	{
+		if (VISUAL)
+			print_stacks(stacks);
+		//ft_printf("NOT SORTED!!!\n");
 	}
 	if (VISUAL)
 		print_stacks(stacks);
-	if (!is_sort_stack(stacks->a))
+	if (size_stack(stacks->a) == 1 && stacks->b)
 	{
-		ft_printf("NOT SORTED!!!\n");
+		pb(stacks);
+		if (!is_sort_stack(stacks->a))
+			ra(stacks);
 	}
-	while (!is_sort_stack(stacks->a))
-		insert_b(stacks, stacks->a->value);
 	while (stacks->b)
 		insert_back_in_a(stacks);
 
 	if (VISUAL)
 		print_stacks(stacks);
-	ft_printf("med_a = %d, med_b = %d\n", stacks->median_a, stacks->median_b);
+	//ft_printf("med_a = %d, med_b = %d\n", stacks->median_a, stacks->median_b);
 	action_print(stacks->actions);
 	return (1);
 }
