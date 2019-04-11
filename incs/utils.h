0067bdc32dddcce917e0471fd3f4f7f48cbc4996
Alexandre PION <apion@student.42.fr>
2019-04-11 15:47:03 +0200

    stack: compute median of stack after a push (pa/pb)

--- incs/utils.h
+++ incs/utils.h
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/03 10:49:58 by apion             #+#    #+#             */
-/*   Updated: 2019/04/11 13:27:38 by apion            ###   ########.fr       */
+/*   Updated: 2019/04/11 15:45:38 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,12 @@ struct	s_data
 {
 	t_stack		*a;
 	t_stack		*b;
+	int			median_a;
+	int			median_b;
 	t_action	*actions;
 };
 
-int			parse_args(int argc, char **argv, t_data *stacks);
+int		parse_args(int argc, char **argv, t_data *stacks);
 
 int		is_sort_stack(t_stack *stack);
 int		free_stacks_and_return(t_data *stacks, int ret);
--- srcs/core/push.c
+++ srcs/core/push.c
@@ -6,7 +6,7 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/03 18:02:58 by apion             #+#    #+#             */
-/*   Updated: 2019/04/11 14:22:56 by apion            ###   ########.fr       */
+/*   Updated: 2019/04/11 15:45:34 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,21 @@
 int		pa(t_data *stacks)
 {
 	action_add_first(&stacks->actions, "pa");
-	return (stack_push(&stacks->b, &stacks->a));
+	if (stack_push(&stacks->b, &stacks->a))
+	{
+		stacks->median_a = get_median(stacks->a);
+		return (1);
+	}
+	return (0);
 }
 
 int		pb(t_data *stacks)
 {
 	action_add_first(&stacks->actions, "pb");
-	return (stack_push(&stacks->a, &stacks->b));
+	if (stack_push(&stacks->a, &stacks->b))
+	{
+		stacks->median_b = get_median(stacks->b);
+		return (1);
+	}
+	return (0);
 }
