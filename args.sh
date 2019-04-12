16506f82f7749f5dfeeef5b57709255bd432d347
Alexandre PION <apion@student.42.fr>
2019-04-12 20:17:35 +0200

    test: improve script for arg creation

--- args.sh
+++ args.sh
@@ -1,1 +1,9 @@
-seq ${1} ${2} | shuf -n ${3} | tr '\n' ' ' | sed 's/ $//'
+#!/bin/sh
+
+if [ $# -ne 3 ]; then
+	echo "usage: $0 {min} {max} {number of values}"
+	exit
+fi
+
+export ARGS=$(seq ${1} ${2} | shuf -n ${3} | tr '\n' ' ' | sed 's/ $//')
+echo $ARGS
