d52ae3f26b62174a6c7d137ba827409b983a6532
Alexandre PION <apion@student.42.fr>
2019-04-12 17:39:56 +0200

    stack: remove median computation on each action

--- srcs/core/push.c
+++ srcs/core/push.c
@@ -6,30 +6,20 @@
 /*   By: apion <apion@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/03 18:02:58 by apion             #+#    #+#             */
-/*   Updated: 2019/04/11 15:45:34 by apion            ###   ########.fr       */
+/*   Updated: 2019/04/12 17:35:42 by apion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
-int		pa(t_data *stacks)
+int			pa(t_data *stacks)
 {
 	action_add_first(&stacks->actions, "pa");
-	if (stack_push(&stacks->b, &stacks->a))
-	{
-		stacks->median_a = get_median(stacks->a);
-		return (1);
-	}
-	return (0);
+	return (stack_push(&stacks->b, &stacks->a));
 }
 
-int		pb(t_data *stacks)
+int			pb(t_data *stacks)
 {
 	action_add_first(&stacks->actions, "pb");
-	if (stack_push(&stacks->a, &stacks->b))
-	{
-		stacks->median_b = get_median(stacks->b);
-		return (1);
-	}
-	return (0);
+	return (stack_push(&stacks->a, &stacks->b));
 }
